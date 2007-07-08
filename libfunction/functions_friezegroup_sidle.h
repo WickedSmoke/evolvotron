6f194c06f4f6f7f728b215dcf05dbb156a13baed
Tim Day <timday@timday.com>
2007-07-08 21:57:45 +0000

    Can't blend or warp sidle.

--- libfunction/functions_friezegroup_sidle.h
+++ libfunction/functions_friezegroup_sidle.h
@@ -45,28 +45,11 @@ struct Sidle
   }
 };
 
-// Don't think this form can be warped without breaking symmetry
-struct SidleInvariant/*
-{
-  SidleInvariant(const Function& f)
-    :_f(f)
-  {}
-  const std::pair<XY,XY> operator()(const XY& p) const
-  {
-    XY d(_f(XYZ(fabs(p.y()),trianglef(p.x(),0.5),0.0)).xy());
-    // Reflect the distortion across symmetry line
-    if (p.y()<0.0) d.y(-d.y());
-    // This is where the distortion will be mapped to by symmetry
-    const real intermediate_y=fabs(p.y()+d.y());
-    // This is what would take us back to the original position
-    const real back_y=fabs(p.y())-intermediate_y;
-    return std::make_pair(d,XY(-d.x(),back_y));
-  }
-private:
-  const Function& _f;
-  }*/;
+//! Can't warp Sidle because the reflection lines lock the pattern down.
+//struct SidleInvariant;
 
-struct SidleBlend;
+//! Can't blend Sidle because there's nothing to hide.
+//struct SidleBlend;
 
 //------------------------------------------------------------------------------------------
 
