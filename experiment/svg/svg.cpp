e36f00e5a099aeb60c808aafad0a25e645e33861
Tim Day <timday@timday.com>
2009-09-01 23:06:25 +0000

    Switch header boilerplate to use headache

--- experiment/svg/svg.cpp
+++ experiment/svg/svg.cpp
@@ -1,71 +1,19 @@
-#include <stdexcept>
-
-#include <QApplication>
-#include <QGLWidget>
-#include <QSvgRenderer>
-
-#include <QtOpenGL/qgl.h>
-
-#include <QtSvg>
-
-// Following file:///usr/share/doc/qt4-doc-html/html/opengl-overpainting.html
-// for combining OpenGL with 2D
-
-// Looks nicer run with e.g __GL_FSAA_MODE=13 ./svg
-
-// TODO: Benchmark.  Try and capture SVG rendering in a display list.
-
-class View : public QGLWidget
-{
-public:
-  View()
-    :QGLWidget()
-    ,_renderer(this)
-  {
-    setAutoFillBackground(false);
-
-    if (!_renderer.load(QString("drawing.svg")))
-      throw std::runtime_error(__PRETTY_FUNCTION__+std::string(": loading SVG file failed\n"));
-  }
-
-  void initializeGL()
-  {}
-
-  //Don't use paintGL when combining 2D/3D
-  //void paintGL()
-  //{}
-
-  void paintEvent(QPaintEvent*) 
-  {
-    glViewport(0,0,static_cast<GLint>(width()),static_cast<GLint>(height()));
-
-    glClearColor(1.0f,1.0f,1.0f,1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    //glEnable(GL_MULTISAMPLE);
-
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-    _renderer.render(&painter);
-    painter.end();
-  }
-protected:
-  void resizeGL(int width,int height)
-  {
-    glViewport(0,0,static_cast<GLint>(width),static_cast<GLint>(height));
-  }
-private:
-  QSvgRenderer _renderer;
-};
-
-int main(int argc,char** argv)
-{
-  QApplication app(argc,argv);
-
-  QWidget*const w=new View();
-
-  w->show();
-
-  return app.exec();
-}
+/**************************************************************************/
+/*  Copyright 2009 Tim Day                                                */
+/*                                                                        */
+/*  This file is part of Evolvotron                                       */
+/*                                                                        */
+/*  Evolvotron is free software: you can redistribute it and/or modify    */
+/*  it under the terms of the GNU General Public License as published by  */
+/*  the Free Software Foundation, either version 3 of the License, or     */
+/*  (at your option) any later version.                                   */
+/*                                                                        */
+/*  Evolvotron is distributed in the hope that it will be useful,         */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/*  GNU General Public License for more details.                          */
+/*                                                                        */
+/*  You should have received a copy of the GNU General Public License     */
+/*  along with Evolvotron.  If not, see <http://www.gnu.org/licenses/>.   */
+/**************************************************************************/
 
