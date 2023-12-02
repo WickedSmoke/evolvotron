32b5c4d0c26b49fdd18f9bdd2f0b3424cfd496be
Karl Robillard <wickedsmoke@users.sourceforge.net>
2023-12-02 14:05:04 -0500

    evolvotron_main.cpp: Fix compile error on Mac OS with Qt 5.7.1.

--- libevolvotron/evolvotron_main.cpp
+++ libevolvotron/evolvotron_main.cpp
@@ -22,6 +22,7 @@
   \todo Eliminate need to include function.h (and instantiate lots of stuff) by moving more into function_node.h/.cpp
 */
 
+#include <QElapsedTimer>
 #include <QSettings>
 
 #include "evolvotron_main.h"
